f4f02109afa37fbc4baecd5765735e7bd5b4139c
xelise-ehmedova-36 <ahmadovaxalisa@gmail.com>
2024-05-23 13:42:02 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// const a=prompt("heftenin gunlerini daxil edin")
+// if (a==1) {
+//     alert("bazar ertesi")
+// }
+//  if (a==2) {
+//     alert("cersenbe axsami")
+// }
+// if (a==3) {
+//     alert("cersenbe")
+// }
+// if (a==4) {
+//     alert("cume axsami")
+// }
+// if (a==5) {
+//     alert("cume")
+// }if (a==6) {
+//     alert("senbe")
+// }
+// if (a==7) {
+//     alert("bazar gunu")
+// }
+
+
+
+// const a=prompt("hava durumunu daxil edin")
+// if (a== "hava yagisli olacag") {
+//     alert("true")
+// }
+// else if (a=="hava yagisli olmayacag") {
+//     alert("true")
+// }
+// else {
+//     alert("false")
+// }
+
+
+
+
+
+
+// const a=prompt("Yasinizi daxil edin")
+// const b=prompt("Teqaud haqqinda melumat daxil edin")
+
+
+// if (a>50 && b==="issiz") {
+//     alert("tam tequd")   
+// }
+// else if (a=50&&b==="issiz") {
+//     alert("yari tequd")
+// }
+// else{
+//     alert("teqaud dusmur")
+// }
+
+// const a=+prompt("Ortalamanizi daxil edin")
+// const b=prompt("aldiginiz netice")
+//  if (a>"90" &&b=="A") {
+//     alert("Imtahandan ugurlu kecdiz")
+//  }if ("81<=a<90" &&b=="B") {
+//     alert("Novbeti merheleye buraxildiz")
+//  }if ("71<=a<80" &&b=="C") {
+//     alert("Yeniden muraciet edin")
+//  }if ("61<=a<70" &&b=="D") {
+//     alert("Kesilmisiniz")
+//  }
+const a=prompt("Istifadeci regemini daxil edin")
+console.log(Math.sqrt(a));
